225a81c235bbda26dca36017464113a06c61e2d2
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-07-16 11:15:02 +0530

    vpc module

--- variables.tf
+++ variables.tf
@@ -25,10 +25,10 @@ variable "igw_tags" {
   default = {}
 }
 
-variable "public_subnet_cidrs" {
+variable "public_subnet_cidr" {
   type = list
   validation{
-    condition = length(var.public_subnet_cidrs) == 2
+    condition = length(var.public_subnet_cidr) == 2
     error_message = "Please provide 2 valid public subnet CIDR"
   }
 }
@@ -42,10 +42,10 @@ variable "private_subnet_tags" {
 }
 
 
-variable "private_subnet_cidrs" {
+variable "private_subnet_cidr" {
   type = list
   validation{
-    condition = length(var.private_subnet_cidrs) == 2
+    condition = length(var.private_subnet_cidr) == 2
     error_message = "Please provide 2 valid private subnet CIDR"
   }
 }
@@ -55,10 +55,10 @@ variable "database_subnet_tags" {
 }
 
 
-variable "database_subnet_cidrs" {
+variable "database_subnet_cidr" {
   type = list
   validation{
-    condition = length(var.database_subnet_cidrs) == 2
+    condition = length(var.database_subnet_cidr) == 2
     error_message = "Please provide 2 valid database subnet CIDR"
   }
 }
